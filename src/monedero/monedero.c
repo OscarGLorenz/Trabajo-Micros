58f5a85556d88a1617151d731ab93b4c4c63fbf3
Óscar García Lorenz <ogl@gomezdemercado.es>
2019-05-07 22:36:34 +0200

    Repo destrozado....

---
+++ src/monedero/monedero.c
@@ -0,0 +1,23 @@
+#include "time.h"
+#include "monedero.h"
+
+// Variables globales deben ser estáticas
+static void (*callback) ();
+static unsigned int tiempo = 0; 
+
+void monederoLoop() {
+	// hago cosas....		
+	if (millis() - tiempo > 1000) {
+		tiempo = millis();
+		callback();  // Persona correcta
+	}
+}
+
+void monederoSetup() {
+  // Bla bla
+}
+
+void monederoSetCallbackCorrecto(void(*f)()) {
+	callback = f;
+}
+
---
+++ src/monedero/monedero.h
@@ -0,0 +1,10 @@
+#ifndef MONEDERO_H_
+#define MONEDERO_H_
+
+void monederoLoop();
+
+void monederoSetup();
+
+void monederoSetCallbackCorrecto(void(*f)());
+
+#endif
