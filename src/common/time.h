f85de609de6ec0210f77589fab5f7b9f54a4a70e
Miguel <miferco97@gmail.com>
2019-05-24 10:17:12 +0200

    time commented

--- src/common/time.h
+++ src/common/time.h
@@ -1,6 +1,25 @@
-//
-// Created by oscar on 3/04/19.
-//
+
+/*************************************************************
+*
+* FICHERO: time.h
+*
+* DESCRIPCIÓN :
+* 	Libreria que permite medir tiempos en microsegundos y en milisegundos empleando el TIMER1.
+*
+* FUNCIONES:
+*
+*		unsigned long millis():
+*			Devuelve los milisegundos que han pasado desde el inicio del programa.
+*
+*		unsigned long micros():
+*			Devuelve los microsegundos que han pasado desde el inicio del programa.
+*
+*		void initTime():
+*			Inicializa el timer para poder usar millis() y micros()
+*
+*
+************************************************************/
+
 
 #ifndef TIME_H_
 #define TIME_H_
