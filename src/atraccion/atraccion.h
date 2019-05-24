5afa3e775d32be1c8d42aca96140787e6c0b40d8
Óscar García Lorenz <ogl@gomezdemercado.es>
2019-05-24 09:27:43 +0200

    Atraccion.h comentada

--- src/atraccion/atraccion.h
+++ src/atraccion/atraccion.h
@@ -1,3 +1,40 @@
+/*************************************************************
+*
+* FICHERO: atraccion.h
+*
+* DESCRIPCIÓN :
+* 	Contiene la funcionalidad de la atracción. Implementada como
+*	máquina de estados
+*
+*
+* FUNCIONES :
+*
+*   void atraccionLoop():
+*      Esta función debe ser llamada continuamente
+*      contiene el grueso del programa y la funcionalidad
+*
+*   void atraccionSetup():
+*      Esta función debe ser llamada al comienzo del programa
+*      inicializa los perifericos del micro y inicializa lo
+*      necesario
+*
+*   void atraccionIniciar():
+*      Esta función debe ser llamada cuando se desea iniciar
+*      la atracción. Solo funcionará si esta no esta funcionando
+*      ni en modo emergencia
+*
+*   void atraccionSetCallbackFinalizado(void(*f)()):
+*      Con esta función se le da a atracción la función que
+*      el programa principal llamará cuando la atracción haya
+*      terminado su proceso y esté lista para otro viaje
+*
+*   void atraccionSetCallbackEmergencia(void(*f)()):
+*      Con esta función se le da a atracción la función que
+*      el programa llamará cuando la atracción haya entrado
+*      en modo emergencia pulsando SW1
+*
+*************************************************************/
+
 #ifndef ATRACCION_H_
 #define ATRACCION_H_
 
