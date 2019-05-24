77a147a21b8b49f4ee52344e669f69d9eaf10578
Óscar García Lorenz <ogl@gomezdemercado.es>
2019-05-24 08:56:14 +0200

    atraccion.c comentado

--- src/atraccion/atraccion.c
+++ src/atraccion/atraccion.c
@@ -1,48 +1,76 @@
-#include "time.h"
-#include "macros.h"
-#include "pinout.h"
-#include "serial.h"
+/*************************************************************
+*
+* FICHERO: atraccion.c
+*
+* DESCRIPCIÓN :
+* 	Contiene la funcionalidad de la atracción. Implementada como
+*	máquina de estados
+*
+*
+* AUTORES:
+*
+* 	Miguel Fernández Cortizas
+*
+*   Óscar García Lorenz
+*
+*
+*************************************************************/
 
-#include "atraccion.h"
-#include <stdint.h>
+#include "time.h"	// Para utilizar millis()
+#include "macros.h"	// Macros auxiliares para manejo de puertos
+#include "pinout.h" // Descripción de interconexiones
+#include "serial.h" // Puerto serie
 
-// Callback de atraccion finalizada
-static void (*callback) ();
+#include "atraccion.h"	// Cabecera del módulo atracción
+#include <stdint.h>		// Tipos estandar de enteros
+
+//#define SERIAL_DEBUG // Salida de datos por el puerto serie
+
+// Guardar callback atraccion finalizada
+static void (*finalizado) ();
 void atraccionSetCallbackFinalizado(void(*f)()) {
-	callback = f;
+	finalizado = f;
 }
 
+// Guardar callback de atraccion en emergencia
 static void (*emergencia) ();
 void atraccionSetCallbackEmergencia(void(*f)()) {
     f = emergencia;
 }
 
-
 // Variables auxiliares para el encoder
-static volatile int16_t pos = 0;
-static volatile uint8_t dir = 0;
-static volatile int32_t now = 0;
-static volatile int32_t past = 0;
-static volatile int32_t dif = 0;
-static volatile int32_t lastIn = 0;
-static volatile int32_t times[5];
-static volatile int16_t bouncing_time=520;
+static volatile int16_t pos = 0;			// posición
+static volatile uint8_t dir = 0;			// dirección
+static volatile int32_t now = 0;			// tiempo actual
+static volatile int32_t past = 0;			// último tiempo
+static volatile int32_t dif = 0;			// diferencia entre tiempos
+static volatile int32_t lastIn = 0;			// último cambio de dirección
+static volatile int32_t times[5];			// últimos 5 valores de dif
+static volatile int16_t bouncing_time=520;	// constante para limitar los cambios de dirección
 
+// Máquina de estados
 enum mode_t {ESPERA,CUELGA,ROTA,GIRA,FRENA,EMERGENCIA,CATASTROFE,LOBOTOMIA,CARGA};
 static enum mode_t mode = ESPERA;
 
-static long int auxTime = 0;
+static long int auxTime = 0; // tiempo auxiliar para controlar cambios de estados
 
+// Esta función se llama desde el main cuando se quiere iniciar la atracción
 void atraccionIniciar() {
+	// Solo si estamos en el estado ESPERA
 	if (mode == ESPERA) {
+		// Pasamos al estado carga
 		mode = CARGA;
+		// Y nos guardamos el tiempo, para esperar 10 segundos después
 		auxTime = millis();
+		// Encendemos el led verde
 		sbi(OUTRUT,L3);
 	}
 }
 
+// Interrupción del encoder
 ISR(SO4_vect){
 
+	// En función de la dirección se incrementa o decrementa la posición
 	if (dir) pos++;
 	else pos--;
 
@@ -91,8 +119,11 @@ ISR(SO4_vect){
 );
 // end asm 1
 */
+	// Nos guardamos el valor de tiempo actual
 	now = millis();
-	volatile uint32_t caca = now;
+	// Hacemos una copia de now
+	volatile uint32_t ahora = now;
+	// Calculamos la diferencia de tiempos
 	dif = now - past;
 /*        asm volatile(
         "push r26 \n"
@@ -132,11 +163,12 @@ ISR(SO4_vect){
         ::"x" (&now),"z" (&past)
 ); dif = now;*/
 
+	// Devolvemos a past el valor de ahora
+    past = ahora;
 
-    past = caca;
 
-
-for (int j=0;j<4;j++)
+	// Hacemos un desplazamiento en times e introducimos al final dif
+	for (int j=0;j<4;j++)
 		times[j]=times[j+1];
 	times[4] = dif;
 
@@ -306,7 +338,10 @@ for (int j=0;j<4;j++)
 );
 
 
+
+    // Si el último cambio de dirección fue hace más tiempo que bouncing time
 	if (now - lastIn > bouncing_time){
+		// Y aux es negativo
 		if (aux < 0){
 
 /*
@@ -318,9 +353,11 @@ for (int j=0;j<4;j++)
 
 */
 
-
+			// Cambiamos de dirección el motor
 			dir = !dir;
-			tbi(OUTRUT,M2_di); // Toggle M2_dir
+			// Toggle M2_dir
+			tbi(OUTRUT,M2_di);
+			// Nos guardamos el valor del tiempo
 			lastIn = now;
 		}
 	}
@@ -328,26 +365,32 @@ for (int j=0;j<4;j++)
 
 }
 
-static volatile bool flag = 0;
-static volatile long int nowInter = 0;
+// Sensor inferior SO5
+static volatile bool flag = 0; // Esta flag se activa con la interrupción
+static volatile long int nowInter = 0;	// Aqui se guarda el tiempo en la interrupción
 ISR(SO5_vect) {
-
+	// Nos aseguramos que el sensor esté a nivel alto
 	if (rbi(PIN_S05,P_S05)) {
-		flag = 1;
-		nowInter = millis();
+		flag = 1;	// Levantamos la flag
+		nowInter = millis(); // Guardamos el tiempo
 	}
 }
 
-static bool L4on = true;
-static unsigned int L4tiempo = 0 ;
+// Led 4
+static bool L4on = true;	// Estado del led
+static unsigned int L4tiempo = 0 ;	// Variable para controlar el parpadeo
+// Esta función se llama periodicamente con un tiempo de on y de off
 void parpadearL4(unsigned int on, unsigned int off) {
+	// En función del estado
 	if (L4on){
+		// Si ha pasado tiempo suficiente apagamos el led
 		if (millis()-L4tiempo > on){
 			L4on = false;
 			cbi(OUTRUT,L4);
 			L4tiempo = millis();
 		}
 	} else{
+		// Si ha pasado tiempo suficiente encendemos el led
 		if (millis()-L4tiempo > off){
 			L4on = true;
 			sbi(OUTRUT,L4);
@@ -356,40 +399,50 @@ void parpadearL4(unsigned int on, unsigned int off) {
 	}
 }
 
+// Esta función es llamada con el main y configura los periféricos necesarios.
 void atraccionSetup() {
+	// Desactivar interrupciones.
 	cli();
 
-	// CHANGE triggers INT3
+	// Configura SO4 como interrupción por flanco de subida y bajada.
 	sbi(CTRL_INT, SO4_C0);
 	cbi(CTRL_INT, SO4_C1);
 
-	// Enable INT3
+	// Activa S04 como interrupción.
 	sbi(INT_MASK, SO4);
 
-	// Enable PCINT0
+	// Activa S05 como PCINT. La máscara y la activación.
 	sbi(S05_CTRL, SO5_ENABLE);
 	sbi(SO5_MASK, SO5);
 
+	// Activar interrupciones
 	sei();
 }
 
+// Función principal de la atracción
 void atraccionLoop() {
-	// FSM
+	// Máquina de estados
 	switch(mode) {
 		case ESPERA:
 		break;
 
 		case CARGA :
+		// Espera 10s para cambiar al modo cuelfa
 		if (millis() - auxTime > 1000 ){
 			mode = CUELGA;
+			// Encendemos motor y apagamos luz.
 			sbi(OUTRUT,M2_en);
 			cbi(OUTRUT,L3);
+			// Nos guardamos el tiempo
 			auxTime= millis();
 		}
 		break;
 
 		case CUELGA:
+		// Cuando la posición sea mayor que 10 pasamos a ROTA
 		if(pos < 10) {
+			// Si seguimos en cuelga, movemos el motor en una dirección 1s
+			// y luego hacemos oscilar al motor con un periodo de 0.6s
 			if (millis() - auxTime < 1000)
 				cbi(OUTRUT,M2_di);
 			else if (millis()%1200 > 1200/2)
@@ -397,6 +450,7 @@ void atraccionLoop() {
 			else
 				cbi(OUTRUT,M2_di);
 		} else {
+			// Se pasa a modo rota, nos guardamos el tiempo
 			mode = ROTA;
 			lastIn = now;
             auxTime= millis();
@@ -404,46 +458,57 @@ void atraccionLoop() {
 		break;
 
 		case ROTA:
+		// Si llevamos 2s sin cambiar de dirección pasa a gira
 		if (millis() - lastIn > 2000) {
 			mode = GIRA;
 		}
-		// WATCHDOG
+		// Si llevamos 100s aqui, pasamos a frena
 		if (millis() - auxTime > 100000 ) {
                 mode = FRENA;
 		}
 		break;
 
 		case GIRA:
+		// Si llevamos 10s aqui pasamosa frena
 		if (millis() - lastIn > 10000) {
 			mode = FRENA;
+			// Cambiamos de dirección para frenar
 			dir = !dir;
 			tbi(OUTRUT,M2_di); // Toggle M2_dir
+			// Nos guardamos el tiempo
 			auxTime = millis();
 		}
 		break;
 
 		case FRENA:
+		// Tras 20s en este estado, pasamos a esperar
 		if (millis() - auxTime > 20000) {
 			mode = ESPERA;
+			// Apagamos el motor
 			cbi(OUTRUT,M2_en);
 			cbi(OUTRUT,M2_di);
-			callback();
+			// Ejecutamos el callback de finalizado para que el main
+			// pueda volver a iniciar la atracción
+			finalizado();
 		}
 		break;
 
 		case CATASTROFE:
+		// Tras 5s en este modo pasamos a emergencia
 		if (millis() - auxTime > 5000) {
 			mode = EMERGENCIA;
 			auxTime = millis();
 		}
 		break;
 
+		// Tras 5s en este modo pasamos a lobotomia
 		case EMERGENCIA:
 		if (millis() - auxTime > 5000) {
 			mode = LOBOTOMIA;
 		}
 		break;
 
+		// En este modo el motor se mantiene apagado
 		case LOBOTOMIA:
 		cbi(OUTRUT,M2_en);
 		cbi(OUTRUT,M2_di);
@@ -451,53 +516,80 @@ void atraccionLoop() {
 
 	}
 
-	// PCINT reseteo de posición, con antirrebote
+	// PCINT(S05) reseteo de posición, con antirrebote
+	// Un tiempo de 1ms, la flag se activa en la interrupción
 	if ( millis() - nowInter > 1 && flag) {
+		// Si sigue en nivel alto el sensor, reseteamos la posición
 		if (rbi(PINB,PB0))
+			// Como el sensor esta descentrado, no reseteamos a 0
 			pos = +6;
+		// Bajamos la flag
 		flag = 0;
 	}
 
-	// Boton de emergencia
+	// Boton de emergencia, activo nivel bajo
 	if (!rbi(PINK,SW1)) {
+		// Si estamos en rota
 		if (mode == ROTA) {
+			// Llamamos al callback de emergencia para cancelar el uso de monedero y tarjetero
             emergencia();
+            // Pasamos al modo emergencia
 			mode = EMERGENCIA;
+			// Cambiamos de dirección para frenar a contra marchar
 			dir = !dir;
 			tbi(OUTRUT,M2_di); // Toggle M2_dir
 			auxTime = millis();
+		// En cuelga o en espera
 		} else if (mode==CUELGA || mode==ESPERA) {
+			// Pasamos al modo lobotomia
 			mode = LOBOTOMIA;
+			// Llamamos al callback de emergencia
             emergencia();
-        } else if(mode == FRENA) {
+		// En frena
+		} else if(mode == FRENA) {
+			// Pasamos al modo lobotomia
 			mode = EMERGENCIA;
-            emergencia();
+			// Llamamos al callback de emergencia
+			emergencia();
+		// En gira
         } else if (mode == GIRA) {
+			// Pasamos al modo catastrofre
 			mode = CATASTROFE;
+			// Cambiamos de dirección para frenar
 			dir = !dir;
 			tbi(OUTRUT,M2_di); // Toggle M2_dir
 			auxTime = millis();
-            emergencia();
+			// Llamamos al callback de emergencia
+			emergencia();
+		// En el modo carga
         } else if (mode == CARGA) {
+			// Pasamos al modo lobotomia
 			mode = LOBOTOMIA;
-			cbi(OUTRUT,L3); // Clear L3
-            emergencia();
+			// Apagamos L3
+			cbi(OUTRUT,L3);
+			// Llamamos al callback de emergencia
+			emergencia();
         }
 	}
 
-	// L4
-	if (mode == CUELGA ||mode == ROTA ||mode == GIRA ||mode == FRENA)
+	// Parpadeo de L4
+	// En los modos siguientes, se mantiene encendido 0.5s y 10s apagado
+	if (mode == CUELGA || mode == ROTA || mode == GIRA || mode == FRENA)
 		parpadearL4(500,10000);
+	// En los modos siguientes se mantiene encendido 0.2s y 1s apagado
 	else if (mode == LOBOTOMIA ||mode == CATASTROFE||mode == EMERGENCIA )
 		parpadearL4(200,1000);
+	// Si no, apagamos el led
 	else {
 		cbi(OUTRUT,L4);
 		L4on = true;
 	}
 
-	// Data dump
+#ifdef SERIAL_DEBUG
+	// Debug por serial, muy cómodo con el plotter de Arduino
 	static long int pocoPoco = 0;
-	if (millis() - pocoPoco > 5 && 0) {
+	// Limitación de tiempo para no saturar el puerto
+	if (millis() - pocoPoco > 5) {
 		pocoPoco = millis();
 		serialPrintLong(dif);
 		serialWrite(' ');
@@ -510,4 +602,5 @@ void atraccionLoop() {
 		serialPrintInt(((int) mode)*100);
 		serialWrite('\n');
 	}
+#endif
 }
